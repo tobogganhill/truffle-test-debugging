8043490a510cd6e441c3139077a5ff1d143e8e0d
ConsenSys Academy <41963038+ConsenSys-Academy@users.noreply.github.com>
2019-01-08 16:29:05 -0500

    Update truffle-config.js

--- project-dir/truffle-config.js
+++ project-dir/truffle-config.js
@@ -15,4 +15,15 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  compilers: {
+    solc: '0.4.25'
+  },  
+  networks: {
+    development: {
+      network_id: '*',
+      port: 8545,
+      host: 'localhost'
+    }
+  }
+
 };
